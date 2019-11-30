1458fc4b2b7e90cb1ae25486503e7767af0e5d95
cliesens27 <42049446+cliesens27@users.noreply.github.com>
2019-12-01 00:43:31 +0100

    Refactor EVERYTHING & add static method to DrawBoardRunner to save current frame to a PNG file

--- src/GUI/DrawingBoardRunner.cs
+++ src/GUI/DrawingBoardRunner.cs
@@ -1,24 +1,58 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
 
 namespace drawingBoard.GUI {
 	public static class DrawingBoardRunner {
+		public static DrawMethod DrawMethod { get; set; } = null;
+		public static double TargetFrameRate { get; set; } = 30;
+		public static int Width { get; private set; } = -1;
+		public static int Height { get; private set; } = -1;
+		public static int X { get; private set; } = -1;
+		public static int Y { get; private set; } = -1;
+		private static DrawingBoard db;
+
 		private static void Init() {
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 		}
 
-		public static DrawingBoard Init(int width, int height, DrawMethod drawMethod) {
+		public static void Init(int width, int height) => Init(width, height, -1, -1);
+
+		public static void Init(int width, int height, int x, int y) {
 			Init();
-			DrawingBoard db = new DrawingBoard(width, height, drawMethod);
-			Application.Run(db);
-			return db;
+
+			Width = width;
+			Height = height;
+			X = x;
+			Y = y;
 		}
 
-		public static DrawingBoard Init(int width, int height, int x, int y, DrawMethod drawMethod) {
-			Init();
-			DrawingBoard db = new DrawingBoard(width, height, x, y, drawMethod);
+		public static void Draw() {
+			if (DrawMethod == null) {
+				throw new Exception("Error, you must set the DrawMethod property before calling Draw()");
+			}
+
+			if (X != -1 && Y != -1) {
+				db = new DrawingBoard(Width, Height, X, Y, DrawMethod);
+			}
+			else {
+				db = new DrawingBoard(Width, Height, DrawMethod);
+			}
+
 			Application.Run(db);
-			return db;
+		}
+
+		public static void SaveToPNG(string path) {
+			Bitmap bitmap = new Bitmap(db.Width, db.Height);
+			db.DrawToBitmap(bitmap, new Rectangle(Point.Empty, db.Size));
+
+			Point p = db.PointToScreen(Point.Empty);
+			Rectangle clientRect = new Rectangle(new Point(p.X - db.Bounds.X, p.Y - db.Bounds.Y), db.ClientSize);
+			Bitmap clientAreBitmap = bitmap.Clone(clientRect, PixelFormat.Format32bppArgb);
+			bitmap.Dispose();
+			clientAreBitmap.Save(path, ImageFormat.Png);
 		}
 	}
 }
--- src/Program.cs
+++ src/Program.cs
@@ -4,9 +4,17 @@ using drawingBoard.GUI;
 namespace drawingBoard {
 	public static class Program {
 		public static void Main() {
-			DrawingBoard db = DrawingBoardRunner.Init(500, 500, (g) => {
+			DrawingBoardRunner.Init(500, 500);
+
+			DrawingBoardRunner.TargetFrameRate = 60;
+
+			DrawingBoardRunner.DrawMethod = (g) => {
 				g.DrawLine(new Pen(Color.Red, 5), 0, 0, 500, 500);
-			});
+
+				DrawingBoardRunner.SaveToPNG(@"C:\Users\Cesarnijunana27\Downloads\test.png");
+			};
+
+			DrawingBoardRunner.Draw();
 		}
 	}
 }
