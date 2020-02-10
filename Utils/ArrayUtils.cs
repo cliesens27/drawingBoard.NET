fc4212725857631b80a2b942dac15f8fc173ad99
cliesens27 <cliesens@gmail.com>
2020-02-10 15:04:00 +0100

    Implement ArrayUtils class that converts int & float arrays to double arrays

---
+++ Utils/ArrayUtils.cs
@@ -0,0 +1,23 @@
+﻿namespace drawingBoard.Utils {
+	public static class ArrayUtils {
+		public static double[] IntToDouble(int[] numbers) {
+			double[] res = new double[numbers.Length];
+
+			for (int i = 0; i < res.Length; i++) {
+				res[i] = numbers[i];
+			}
+
+			return res;
+		}
+
+		public static double[] FloatToDouble(float[] numbers) {
+			double[] res = new double[numbers.Length];
+
+			for (int i = 0; i < res.Length; i++) {
+				res[i] = numbers[i];
+			}
+
+			return res;
+		}
+	}
+}
