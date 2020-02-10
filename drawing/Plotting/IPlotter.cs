733530a73ebc00618e180da93e798e3ea790538b
cliesens27 <cliesens@gmail.com>
2020-02-10 01:42:34 +0100

    Rearrange IPlotter member definitions

--- drawing/Plotting/IPlotter.cs
+++ drawing/Plotting/IPlotter.cs
@@ -10,52 +10,52 @@ namespace drawingBoard.Drawing.Plotting {
 		protected float zeroX;
 		protected float zeroY;
 
-		protected void InitPlot(DrawingBoard db, int[] xs, int[] ys) {
-		}
-
 		protected void InitPlot(DrawingBoard db, int[] xs, int[] ys, int x, int y, int width, int height) {
-		}
-
-		protected void InitPlot(DrawingBoard db, float[] xs, float[] ys) {
 		}
 
 		protected void InitPlot(DrawingBoard db, float[] xs, float[] ys, int x, int y, int width, int height) {
 		}
 
-		protected void InitPlot(DrawingBoard db, double[] xs, double[] ys) {
-		}
-
 		protected void InitPlot(DrawingBoard db, double[] xs, double[] ys, int x, int y, int width, int height) {
 		}
 
-		public abstract void Plot(DrawingBoard db, int[] xs, int[] ys);
+		protected void InitPlot(DrawingBoard db, float[] xs, float[] ys)
+			=> InitPlot(db, xs, ys, 0, 0, db.Width, db.Height);
 
-		public abstract void Plot(DrawingBoard db, int[] xs, int[] ys, int x, int y, int width, int height);
+		protected void InitPlot(DrawingBoard db, int[] xs, int[] ys)
+			=> InitPlot(db, xs, ys, 0, 0, db.Width, db.Height);
+
+		protected void InitPlot(DrawingBoard db, double[] xs, double[] ys)
+			=> InitPlot(db, xs, ys, 0, 0, db.Width, db.Height);
+
+		public abstract void Plot(DrawingBoard db, int[] xs, int[] ys);
 
 		public abstract void Plot(DrawingBoard db, float[] xs, float[] ys);
 
-		public abstract void Plot(DrawingBoard db, float[] xs, float[] ys, int x, int y, int width, int height);
-
 		public abstract void Plot(DrawingBoard db, double[] xs, double[] ys);
+
+		public abstract void Plot(DrawingBoard db, int[] xs, int[] ys, int x, int y, int width, int height);
+
+		public abstract void Plot(DrawingBoard db, float[] xs, float[] ys, int x, int y, int width, int height);
 
 		public abstract void Plot(DrawingBoard db, double[] xs, double[] ys, int x, int y, int width, int height);
 
 		public void Plot(DrawingBoard db, List<int> xs, List<int> ys)
 			=> Plot(db, xs.ToArray(), ys.ToArray());
 
-		public void Plot(DrawingBoard db, List<int> xs, List<int> ys, int x, int y, int width, int height)
-			=> Plot(db, xs.ToArray(), ys.ToArray(), x, y, width, height);
-
 		public void Plot(DrawingBoard db, List<float> xs, List<float> ys)
 			=> Plot(db, xs.ToArray(), ys.ToArray());
-
-		public void Plot(DrawingBoard db, List<float> xs, List<float> ys, int x, int y, int width, int height)
-			=> Plot(db, xs.ToArray(), ys.ToArray(), x, y, width, height);
 
 		public void Plot(DrawingBoard db, List<double> xs, List<double> ys)
 			=> Plot(db, xs.ToArray(), ys.ToArray());
 
-		public void Plot(DrawingBoard db, List<double> xs, List<double> ys, int x, int y, int width, int height)
+		public void Plot(DrawingBoard db, List<int> xs, List<int> ys, int x, int y, int width, int height)
 			=> Plot(db, xs.ToArray(), ys.ToArray(), x, y, width, height);
+
+		public void Plot(DrawingBoard db, List<float> xs, List<float> ys, int x, int y, int width, int height)
+			=> Plot(db, xs.ToArray(), ys.ToArray(), x, y, width, height);
+
+		public void Plot(DrawingBoard db, List<double> xs, List<double> ys, int x, int y, int width, int height)
+		=> Plot(db, xs.ToArray(), ys.ToArray(), x, y, width, height);
 	}
 }
