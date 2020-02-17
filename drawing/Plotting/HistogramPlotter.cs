fbcc1f1d25fd63a85b1ee7014b4d8f5065392786
cliesens27 <cliesens@gmail.com>
2020-02-17 22:42:46 +0100

    Fix various bugs & finish implementing HistogramPlotter

--- drawing/Plotting/HistogramPlotter.cs
+++ drawing/Plotting/HistogramPlotter.cs
@@ -9,51 +9,69 @@ namespace drawingBoard.Drawing.Plotting {
 		private const int MAX_NB_BINS = 20;
 
 		public override void Plot(DrawingBoard db, double[] xs, double[] ys)
-			=> Plot(db, xs, ys, 0, 0, db.Width, db.Height);
+			=> throw new NotImplementedException();
 
 		public override void Plot(DrawingBoard db, double[] xs, double[] ys,
-			int x, int y, int width, int height) {
-			int nbBins = ComputeNbBins(xs);
+			int x, int y, int width, int height)
+			=> throw new NotImplementedException();
 
-			Plot(db, xs, ys, x, y, width, height, nbBins);
+		public void Plot(DrawingBoard db, double[] xs)
+			=> Plot(db, xs, 0, 0, db.Width, db.Height);
+
+		public void Plot(DrawingBoard db, double[] data, int x, int y, int width, int height) {
+			int nbBins = ComputeNbBins(data);
+
+			Plot(db, data, x, y, width, height, nbBins);
 		}
 
-		public void Plot(DrawingBoard db, double[] xs, double[] ys, int nbBins)
-			=> Plot(db, xs, ys, 0, 0, db.Width, db.Height);
+		public void Plot(DrawingBoard db, double[] data, int nbBins)
+			=> Plot(db, data, 0, 0, db.Width, db.Height, nbBins);
 
-		public void Plot(DrawingBoard db, double[] xs, double[] ys,
-			int x, int y, int width, int height, int nbBins) {
-			if (xs.Length < 2 * MIN_NB_BINS) {
-				throw new ArgumentException($"X has too few entries ({xs.Length}) to plot a histogram\n\t" +
+		public void Plot(DrawingBoard db, double[] data, int x, int y,
+			int width, int height, int nbBins) {
+			if (data.Length < 2 * MIN_NB_BINS) {
+				throw new ArgumentException($"X has too few entries ({data.Length}) to plot a histogram\n\t" +
 					$"X.Length should be at least {2 * MIN_NB_BINS}");
 			}
 
-			InitPlot(db, xs, ys, x, y, width, height);
+			double[] xs = new double[nbBins + 1];
+			double[] ys = new double[nbBins + 1];
 
-			db.Stroke(0);
-			db.Fill(235);
-			db.StrokeWidth(1);
-			db.RectMode(RectangleMode.CORNER);
-
-			int[] counts = ComputeCounts(ys, nbBins);
-			float colWidth = (axesBounds.Right - axesBounds.Left) / nbBins;
-			float maxCount = counts.Max() / (float) xs.Length;
+			minMaxY = Utils.ArrayUtils.FindMinMax(data);
+			int[] counts = ComputeCounts(data, nbBins);
+			double incr = (minMaxY.max - minMaxY.min) / nbBins;
 
 			for (int i = 0; i < nbBins; i++) {
-				float val = counts[i] / (float) xs.Length;
-				float screenX = SpecialFunctions.Lerp(i, 0, nbBins, axesBounds.Left, axesBounds.Right);
-				float screenY = SpecialFunctions.Lerp(val, 0, maxCount, axesBounds.Bottom, axesBounds.Top);
+				xs[i] = minMaxY.min + i * incr;
+				ys[i] = counts[i] / (double) data.Length;
+			}
 
-				db.Rectangle(screenX, axesBounds.Y + axesBounds.Height - screenY, colWidth, screenY);
+			xs[nbBins] = minMaxY.max + incr;
+
+			InitPlot(db, xs, ys, x, y, width, height);
+
+			db.RectMode(RectangleMode.CORNER);
+			db.Stroke(0);
+			db.Fill(245);
+			db.StrokeWidth(1);
+
+			float colWidth = (axesBounds.Right - axesBounds.Left) / nbBins;
+			float maxCount = counts.Max();
+
+			for (int i = 0; i < nbBins; i++) {
+				float screenX = SpecialFunctions.Lerp(i, 0, nbBins, axesBounds.Left, axesBounds.Right);
+				float screenY = (float) SpecialFunctions.Lerp(ys[i], 0, maxCount / data.Length, 0, axesBounds.Height);
+
+				db.Rectangle(screenX, axesBounds.Bottom - screenY, colWidth, screenY);
 			}
 		}
 
-		private int[] ComputeCounts(double[] ys, int nbBins) {
+		private int[] ComputeCounts(double[] data, int nbBins) {
 			double incr = (minMaxY.max - minMaxY.min) / nbBins;
 			int[] counts = new int[nbBins];
 
-			for (int i = 0; i < ys.Length; i++) {
-				double val = ys[i];
+			for (int i = 0; i < data.Length; i++) {
+				double val = data[i];
 
 				for (int j = 0; j < nbBins; j++) {
 					double from = minMaxY.min + j * incr;
@@ -69,10 +87,10 @@ namespace drawingBoard.Drawing.Plotting {
 			return counts;
 		}
 
-		private int ComputeNbBins(double[] xs) {
+		private int ComputeNbBins(double[] data) {
 			//int n = (int) Math.Sqrt(xs.Length);
 			//int n = 1 + (int) Math.Log(xs.Length, 2);
-			int n = (int) (2 * Math.Pow(xs.Length, 1.0 / 3.0));
+			int n = (int) (2 * Math.Pow(data.Length, 1.0 / 3.0));
 
 			n = n > MAX_NB_BINS ? MAX_NB_BINS : n;
 			n = n < MIN_NB_BINS ? MIN_NB_BINS : n;
