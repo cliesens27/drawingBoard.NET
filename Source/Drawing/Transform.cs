7c485a952b5a05bb61b4d1d701ab657691ecf363
CL <cliesens@gmail.com>
2024-10-28 10:30:07 +0100

    Add .editorconfig and normalize line endings

--- Source/Drawing/Transform.cs
+++ Source/Drawing/Transform.cs
@@ -4,9 +4,9 @@ namespace DrawingBoardNET.Drawing;
 
 internal struct Transform
 {
-	internal TransformType Type { get; }
+    internal TransformType Type { get; }
 
-	internal double Value { get; }
+    internal double Value { get; }
 
-	public Transform(TransformType type, double value) => (Type, Value) = (type, value);
+    public Transform(TransformType type, double value) => (Type, Value) = (type, value);
 }
